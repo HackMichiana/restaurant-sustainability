e76c1ea56129ab71f2a9f6973f2c37a3900513d3
trwiley <taylor.r.wiley@gmail.com>
2018-02-27 23:11:25 -0500

    Created an R script to filter the business licenses to restaurants only and relevant variables; created a .gitignore and the restaurants file.

---
+++ restaurant_filtering.R
@@ -0,0 +1,24 @@
+#Filters the dataset down to restaurants with active licences.
+
+Restaurant_Licenses <- subset(Business_Licenses, (Business_Licenses$Classification_Code == "RESTAM" | Business_Licenses$Classification_Code == "RESTZ"))
+
+# Filter out nonactive licenses
+
+Current_Licenses <- subset(Restaurant_Licenses, Restaurant_Licenses$License_Status != "IN" & Restaurant_Licenses$License_Status != "VO")
+
+#Grab only the attributes we want. 
+
+Final_Restaurant_Set <- data.frame(
+  Name = Current_Licenses$Business_Name,
+  Address = Current_Licenses$Street_Address,
+  City = Current_Licenses$City,
+  State = Current_Licenses$State,
+  Zip = Current_Licenses$Zip,
+  Phone_Number = Current_Licenses$Business_Phone_Number
+  
+)
+
+#Write to CSV.
+
+write.csv(Final_Restaurant_Set, "restaurants.csv")
+
